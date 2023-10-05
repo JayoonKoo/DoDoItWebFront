2747046d7cd3fd9745227dc3374ba05fcf8a0547
jayoonkoo <rnwkdbs12@gmail.com>
2023-10-06 01:03:33 +0900

    feat: InputField 로 변경
    TODO :
    - button 추가
    - smallLink 추가
    - form 추가
    - 카드 추가

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,47 +1,26 @@
 import React from 'react';
+import Logo from '../components/atom/Logo';
+import InputField from '../components/organisms/InputField';
 
 const SignupPage = () => {
   return (
     <div className="bg-gray-50 h-screen flex justify-center items-center">
       <div className="bg-white p-16 rounded-md shadow-sm relative">
         <div className="absolute left-1/2 -translate-x-1/2 -top-24">
-          <h1 className="text-4xl font-semibold">
-            Front<span className="text-green-500">App</span>
-          </h1>
+          <Logo />
         </div>
 
         <form className="grid grid-cols-1 gap-4">
           <div className="flex flex-col">
-            <label htmlFor="id" className="font-semibold text-sm">
-              아이디
-            </label>
-            <input
-              id="id"
-              type="text"
-              className="w-96 border rounded-md focus:ring-1 focus:ring-green-200 focus:outline-none px-4 py-2"
-            />
+            <InputField id="id" type="text" label="아이디" />
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="password" className="font-semibold text-sm">
-              비밀번호
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-96 border rounded-md focus:ring-1 focus:ring-green-200 focus:outline-none px-4 py-2"
-            />
+            <InputField id="password" type="password" label="비밀번호" />
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="nickname" className="font-semibold text-sm">
-              닉네임
-            </label>
-            <input
-              id="nickname"
-              type="text"
-              className="w-96 border rounded-md focus:ring-1 focus:ring-green-200 focus:outline-none px-4 py-2"
-            />
+            <InputField id="nickname" type="text" label="닉네임" />
           </div>
 
           <div className="mt-2">
